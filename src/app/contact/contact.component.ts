28dd43d8673b05c1cf639e9f7d14fe06efae82ac
Priscilla Li <sibeili@hotmail.com>
2018-07-29 21:19:02 -0400

    Promises

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 
@@ -8,6 +8,8 @@ import { Feedback, ContactType } from '../shared/feedback';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  @ViewChild('fform') feedbackFormDirective;
 
   feedbackForm: FormGroup;
   feedback: Feedback;
@@ -22,13 +24,13 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: null,
-      email: '',
+      firstname: ['', Validators.required ],
+      lastname: ['', Validators.required ],
+      telnum: [null, Validators.required ],
+      email: ['', Validators.required ],
       agree: false,
       contacttype: 'None',
-      message: ''
+      message: '',
     });
   }
 
@@ -39,7 +41,16 @@ export class ContactComponent implements OnInit {
     }
     console.log(this.feedback);
     console.log(this.feedback.agree);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: null,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: '',
+    });
+    this.feedbackFormDirective.resetForm();
   }
 
 }
